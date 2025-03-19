ec1927edc1ca2564bcfc5a85743e52792b133a24
Balajitv12 <balajitv2004@gmail.com>
2025-03-19 07:10:58 +0530

    home file

---
+++ 02_variables.php
@@ -0,0 +1,16 @@
+<?php 
+  $name ="balaji";//string
+  $age = 40;//int
+  $father_name ='Velu';//string
+  $mother_name ='lather';//string
+  $sister_name ='nirmala';//string
+  $brother_name = 'nithish';//string
+  $has_kids = false;//bool
+  $cash_on_hand = 320.75;//float
+  //print_r($name);
+  //print($age);
+  //echo $mother_name;
+  //echo $name  . ' is ' .  $age  . ' years old';
+  //echo "$name is $age years olds";
+  echo "${name} is ${age} years old"
+?>
---
+++ balaji.php
@@ -0,0 +1,19 @@
+<?php
+ //echo"Hello13"; 
+  //echo 'baljai';
+  //echo [1,2,3];
+  //print_r([1,2,3,4,5]);
+  //var_dump(1+2);
+  //var_dump(true);
+  //var_export("hello")
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+  </head>
+  <body>
+    <h1><?=  'Post One'; ?></h1>
+  </body>
+</html>
